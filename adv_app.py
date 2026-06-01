18a319d791e08727ee1e193d1017607d835b21fd
rajetejas7 <tejasr777@gmail.com>
2026-06-01 19:30:33 +0530

    Add files via upload

---
+++ adv_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+import numpy as np
+model=pickle.load(open('lr.sav','rb'))
+st.title('Sales Prediction App')
+#Input features
+
+TV=st.number_input('TV adv budget',min_value=0.0)
+Radio=st.number_input('Radio Adv Budget',min_value=0.0)
+Newspaper=st.number_input('newspaper budget',min_value=0.0)
+if st.button('predict sales'):
+  input_data=np.array([[TV,Radio,Newspaper]])
+  Prediction=model.predict(input_data)[0]
+  st.success(f'predicted Sales:{Prediction:.2f}')
